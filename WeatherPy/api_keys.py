962f9a45a3e868ba114cb215c38b30a7dd396b42
wsylliac <whitney@Whitneys-MacBook-Pro.local>
2023-12-14 00:36:16 -0500

    updating hw

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e661091055832c8766bac35e63348539"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "8946e2be541249a28056bf521f32ab5b"
